f4451738f0a5a6cd8b05b1d33dc26924d59ed5ca
Mohamed Hussein <105607296+MohamedHussein43@users.noreply.github.com>
2024-01-17 16:20:11 +0200

    creating lang directory then add en and ar for multi language and rewrite the login strings to make it come from the en file, adding base lauout file but without any code just creating it, remove some unwanted logos and strings from dashboard and navegation-menu blades

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,10 @@
+<?php
+return[
+    'email' =>'Email',
+    'password' =>'Password',
+    'remember_me' =>'Remember me',
+    'forgot_password' =>'Forgot your Password?',
+    'login' =>'Login',
+    'dont_have_account' =>'Don\'t have an account?',
+    'sine_up' =>'Sine Up',
+];
